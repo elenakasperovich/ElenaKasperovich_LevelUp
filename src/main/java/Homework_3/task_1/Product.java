c24f81947544cc1f9aec65bccfd8d7b10a839c19
ekasperovich <elenakasperovich@ya.ru>
2021-09-08 10:29:16 +0300

    Homework_3.task_1.KasperovichElena_2attempt

--- src/main/java/Homework_3/task_1/Product.java
+++ src/main/java/Homework_3/task_1/Product.java
@@ -1,8 +1,8 @@
 package Homework_3.task_1;
 
 public class Product {
-    String name;
-    int calories;
+    private String name;
+    private int calories;
 
     public Product(String name, int calories) {
         this.name = getProductName(name);
@@ -12,5 +12,13 @@ public class Product {
     // пример инкапсуляции
     private String getProductName(String name) {
         return "Продукт " + name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCalories() {
+        return calories;
     }
 }
--- src/main/java/Homework_3/task_1/Sheff.java
+++ src/main/java/Homework_3/task_1/Sheff.java
@@ -6,7 +6,7 @@ public class Sheff implements Cook {
     public Dish cook(Product[] products, String nameOfDish) {
         int sumCalories = 0;
         for (Product product : products) {
-            sumCalories = sumCalories + product.calories;
+            sumCalories = sumCalories + product.getCalories();
         }
         System.out.println("Блюдо " + nameOfDish + " готово. Ккал на 100г = " + sumCalories);
         return new Dish(nameOfDish, sumCalories);
